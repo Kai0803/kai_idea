51eca3584cd8b5f979d7aeded6d97ecd1c900902
Hoddy <work@LAPTOP-U4U1KOVA>
2024-08-22 23:20:11 +0800

    add data to Sdata

--- Sdata.py
+++ Sdata.py
@@ -1,2 +1,4 @@
-API_KEY = '6iVbZJ2Kiuj4ZER3LeiWo8scritVBqQrE933XMcHYhpKuVDseiTrz6pgJtFvdFfhuM'
-SECRET = 'c7pyWoMuXifQ1X2kdAkZ1GwTDtrY67BT4vOAHfIY15ByEwxOCMsTlrTE0V7mf5XJ'
+API_KEY = '6iVbZJ2Kiuj4ZER3LeiWo8scritVBqQrE933XMcHYhpKuVDseiTrz6pgJtFvdFfhuM'#pionex
+SECRET = 'c7pyWoMuXifQ1X2kdAkZ1GwTDtrY67BT4vOAHfIY15ByEwxOCMsTlrTE0V7mf5XJ'#pionex
+bybitkey = 'toSfK21vtEl6OGV5Fx'
+bybit_Secret = 'hlgi8AxLtrC9jsOIOHodXEX4ihf0uCCkFNWk'
