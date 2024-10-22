8bfe1d366c66d4da0e3743e57ed615404c6823bc
Hoddy <work@LAPTOP-U4U1KOVA>
2024-10-22 23:29:06 +0800

    add api put test

--- trading_api/models.py
+++ trading_api/models.py
@@ -11,7 +11,8 @@ curl -X POST http://localhost:8000/api/strategy-info/ \
 --------------------------------
 """
 class StrategyInfo(models.Model):
-    timestamp = models.DateTimeField(auto_now_add=True)
+#     timestamp = models.DateTimeField(auto_now_add=True)#只是用第一次時間
+    timestamp = models.DateTimeField(auto_now=True)#有更新就覆蓋新的時間
     strategy_name = models.CharField(max_length=100)
     current_position = models.FloatField()
     profit_loss = models.FloatField()
