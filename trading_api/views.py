fc717cfed8a1093c24c403c8b05d6daa82f682c8
Hoddy <work@LAPTOP-U4U1KOVA>
2024-10-23 22:34:05 +0800

    add ui for position

--- trading_api/views.py
+++ trading_api/views.py
@@ -4,7 +4,17 @@ from .models import StrategyInfo
 from .serializers import StrategyInfoSerializer
 from rest_framework.authentication import TokenAuthentication,SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
-
+from django.shortcuts import render
+from django.http import JsonResponse
+from bybit_test2 import get_real_time_position
+def real_time_position(request):
+       position = get_real_time_position()
+       if position:
+           return JsonResponse(position)
+       else:
+           return JsonResponse({'error': '無法獲取倉位信息'}, status=400)
+def trading_dashboard(request):
+    return render(request, 'trading_api/trading_dashboard.html')
 class StrategyInfoViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication,SessionAuthentication]
     permission_classes = [IsAuthenticated]
